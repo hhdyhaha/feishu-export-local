0e347a9cc26d4958b2197d1da1d732bcd0847abe
hhdyhaha <1164549585@qq.com>
2024-08-26 21:09:43 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -170,6 +170,16 @@ function downloadFile(file_token: string, headers: object, fileName: string, fil
       </el-button
       >
     </div>
+    <el-card>
+      <div>
+        注意，标题里不能带‘开’，否则会下载失败；注意，标题里不能带‘开’，否则会下载失败；注意，标题里不能带‘开’，否则会下载失败（我使用postman和python发送请求都能下载，但是axios只要标题带“开”字，就下载不了，后续有时间再解决！）
+      </div>
+      <div>
+        去下面链接获取到user_access_token，放到运行起来的项目中，点击下载
+      </div>
+
+      <a href="https://open.feishu.cn/api-explorer/cli_a52b4df38b7b500c?apiName=create&project=authen&resource=oidc.access_token&version=v1" target="_blank">飞书开发者平台</a>
+    </el-card>
 
   </el-card>
 </template>
